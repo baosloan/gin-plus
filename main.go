74bbfea5fd8765aed94ba3b88c0d7863895b749b
baosloan <baosloan@gmail.com>
2024-07-29 22:47:18 +0800

    import gin framework

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf(`r.Run(":8080") failed: %v`, err)
+	}
+}
